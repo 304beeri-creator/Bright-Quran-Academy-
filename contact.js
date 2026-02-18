178721f0a5d59cc54b1df85e5ec03f14fa6e2a65
304beeri-creator <304beeri@gmail.com>
2026-02-18 06:38:39 +0500

    Delete contact.js

--- contact.js
+++
@@ -1,3 +0,0 @@
-export const WHATSAPP_NUMBER = '923354012338';
-export const WHATSAPP_MESSAGE = 'I am interested in Quran classes at Bright Quran Academy.';
-export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(WHATSAPP_MESSAGE)}`;
